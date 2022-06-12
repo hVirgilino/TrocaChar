64717ca675566c8ff3080c9981d4144f68aecc5f
Henrique Virgilino <henriquevirgilino@ucl.br>
2022-06-12 13:22:26 -0300

    Implementação da lógica do código-fonte da biblioteca

--- TrocaChar.c
+++ TrocaChar.c
@@ -4,6 +4,22 @@
 
 int TrocaChar(char **msgAntes, int qtdCharVet, char charAntes, char charDepois, char **msgDepois){
 
+    int qtdTrocas = 0;
+    *msgDepois = (char *)malloc (qtdCharVet * sizeof(char));
 
+    for (int i = 0; i <= qtdCharVet; i++)
+    {
+        (*msgDepois)[i] = (*msgAntes)[i];
+
+        if ((*msgDepois)[i] == charAntes)
+        {
+            (*msgDepois)[i] = charDepois;
+
+            qtdTrocas++;
+        }
+
+    }
+
+    return qtdTrocas;
 
 }
