8fcb1e89c64495f597b2db85c5adf87e77c4d999
Arthur <arthuroliveira@ucl.br>
2022-06-12 13:24:24 -0300

    Implementação no código cliente

--- main.c
+++ main.c
@@ -5,6 +5,23 @@
 
 int main(){
 
+    int qtdTrocas;
+    char charAntes[1];
+    char charDepois[1];
+    char *msgAntes;
+    char *msgDepois;
+
+    msgAntes = (char *)malloc (256 * sizeof(char));
+
+    scanf("%s", msgAntes);
+    scanf(" %c", charAntes);
+    scanf(" %c", charDepois);
+
+    qtdTrocas = TrocaChar(&msgAntes, strlen(msgAntes), charAntes[0], charDepois[0], &msgDepois);
+
+    printf("%s", msgDepois);
+    printf("\n%s", msgAntes);
+    printf("\n%d", qtdTrocas);
 
     return 0;
 }
