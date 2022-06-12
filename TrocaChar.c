4989ce3857a0c19cb89234096d3c919158d34f56
Henrique Virgilino <henriquevirgilino@ucl.br>
2022-06-12 13:28:14 -0300

    Adição de comentários e compilação de arquivos

--- TrocaChar.c
+++ TrocaChar.c
@@ -5,21 +5,27 @@
 int TrocaChar(char **msgAntes, int qtdCharVet, char charAntes, char charDepois, char **msgDepois){
 
     int qtdTrocas = 0;
+
+    //Aloca para o tamanho da string de saída ser do tamanho da de entrada
     *msgDepois = (char *)malloc (qtdCharVet * sizeof(char));
 
     for (int i = 0; i <= qtdCharVet; i++)
     {
+        //Constrói a string de saída de acordo com os caracteres da de entrada
         (*msgDepois)[i] = (*msgAntes)[i];
 
+        //Caso um desses caracteres seja o definido, haverá a troca
         if ((*msgDepois)[i] == charAntes)
         {
             (*msgDepois)[i] = charDepois;
 
+            //Aumenta o contador de trocas feitas
             qtdTrocas++;
         }
 
     }
-
+    
+    //Retorna o número de trocas, a string de saída já definida na main será alterada pelo ponteiro
     return qtdTrocas;
 
 }
--- main.c
+++ main.c
@@ -5,20 +5,25 @@
 
 int main(){
 
+    //Declaração de variáveis
     int qtdTrocas;
     char charAntes[1];
     char charDepois[1];
     char *msgAntes;
     char *msgDepois;
 
+    //Aloca um tamanho de 256 bytes para o tamanho da string de entrada
     msgAntes = (char *)malloc (256 * sizeof(char));
 
+    //Recebe as variáveis
     scanf("%s", msgAntes);
     scanf(" %c", charAntes);
     scanf(" %c", charDepois);
 
+    //Chama a função
     qtdTrocas = TrocaChar(&msgAntes, strlen(msgAntes), charAntes[0], charDepois[0], &msgDepois);
 
+    //Imprime o resultado
     printf("%s", msgDepois);
     printf("\n%s", msgAntes);
     printf("\n%d", qtdTrocas);
