38214196dc66557065a44ec7a4d29513160ea0a9
Arthur <arthuroliveira@ucl.br>
2022-06-12 13:19:48 -0300

    Criando arquivos e declarações de funções

---
+++ TrocaChar.c
@@ -0,0 +1,9 @@
+#include "TrocaChar.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int TrocaChar(char **msgAntes, int qtdCharVet, char charAntes, char charDepois, char **msgDepois){
+
+
+
+}
---
+++ TrocaChar.h
@@ -0,0 +1,9 @@
+#ifndef TROCACHAR_H
+
+#define TROCACHAR_H
+
+
+int TrocaChar(char **msgAntes, int qtdCharVet, char charAntes, char charDepois, char **msgDepois);
+
+
+#endif
